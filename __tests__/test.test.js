1606cc893f8b6e16bdd04cf38f3f64fd604b508c
Anna Norman <agibbard70@hotmail.com>
2022-11-18 16:11:08 +0000

    11 complete

--- __tests__/test.test.js
+++ __tests__/test.test.js
@@ -183,10 +183,30 @@ describe("GET /api/articles/:article_id", () => {
           topic: "mitch",
           created_at: "2020-07-09T20:11:00.000Z",
           votes: 100,
+          comment_count: 11,
         });
       });
   });
 });
+test("status:200, responds with a single matching article and a comment count of 0 when passed a valid article_id with no comments", () => {
+  const article_id = 2;
+  return request(app)
+    .get(`/api/articles/${article_id}`)
+    .expect(200)
+    .then(({ body }) => {
+      expect(body.article).toEqual({
+        article_id: 2,
+        author: "icellusedkars",
+        title: "Sony Vaio; or, The Laptop",
+        body: "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
+        topic: "mitch",
+        created_at: expect.any(String),
+        votes: 0,
+        comment_count: 0,
+      });
+    });
+});
+
 test("status:400, responds with an error message when passed a bad article ID", () => {
   return request(app)
     .get("/api/articles/notAnID")
